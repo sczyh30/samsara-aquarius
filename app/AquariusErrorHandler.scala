16dc23d5f5f847368ef78fe112eacba11a85483b
sczyh30 <sczyh16@gmail.com>
2016-05-03 00:51:00 +0800

    small update about error handler and filter config

--- app/AquariusErrorHandler.scala
+++ app/AquariusErrorHandler.scala
@@ -1,12 +1,13 @@
-import javax.inject.{Provider, Inject}
+import javax.inject.{Inject, Provider}
 
 import play.api.mvc.RequestHeader
 import play.api.mvc.Results._
 import play.api.routing.Router
-import play.api.{OptionalSourceMapper, Configuration, Environment}
-import play.api.http.DefaultHttpErrorHandler
+import play.api._
+import play.api.http.{DefaultHttpErrorHandler, HttpErrorHandlerExceptions}
 
 import scala.concurrent.Future
+import scala.util.control.NonFatal
 
 /**
   * Samsara Aquarius
@@ -27,7 +28,28 @@ class AquariusErrorHandler @Inject()(env: Environment,
   }
 
   override def onBadRequest(request: RequestHeader, message: String) = {
-    //Future.successful(BadRequest(views.html.error.ServerError("Oops", "出了点问题...自函子范畴上的幺半群正在变换中...")(request)))
-    Future.successful(NotFound(views.html.error.NotFound()(request)))
+    Future.successful(NotFound(views.html.error.NotFound()(request))) // 400 -> 404
+  }
+
+  override def onServerError(request: RequestHeader, exception: Throwable) = {
+    try {
+      val usefulException = HttpErrorHandlerExceptions.throwableToUsefulException(sourceMapper.sourceMapper,
+        env.mode == Mode.Prod, exception)
+
+      logServerError(request, usefulException)
+
+      env.mode match {
+        case Mode.Prod => onProdServerError(request)
+        case _ => onDevServerError(request, usefulException)
+      }
+    } catch {
+      case NonFatal(e) =>
+        Logger.error("Error while handling error", e)
+        Future.successful(InternalServerError)
+    }
+  }
+
+  def onProdServerError(request: RequestHeader) = {
+    Future.successful(InternalServerError(views.html.error.ServerError("Oops...", "服务器出了点小问题。。。")(request)))
   }
 }
